20452aee06dbc6abaf724f8e1e83a7501e952851
Nikita Rogalenko <nikitalr@mail.ru>
2018-10-08 14:46:21 +0300

    RogCSS disappearing TimoFixed

--- RogoPip2/src/ControllerServlet.java
+++ RogoPip2/src/ControllerServlet.java
@@ -4,32 +4,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
 
 @WebServlet("/")
 public class ControllerServlet extends HttpServlet {
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
         {
-            String query = request.getQueryString();
-            if (query == null) {
-                response.sendRedirect("FormPage.jsp");
+            String query = request.getRequestURI();
+
+            if (query != null && (query.endsWith(".css")||query.endsWith(".png"))) {
+                if (query.endsWith(".css")) response.setContentType("text/css;charset=UTF-8");
+                if (query.endsWith(".png")) response.setContentType("image/png");
+                String filename = query.split("/")[query.split("/").length - 1];
+                InputStream res = getServletContext().getResourceAsStream("/" + filename);
+                if (res == null) {
+                    response.setStatus(404);
+                } else {
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = res.read(buffer)) != -1) {
+                        response.getOutputStream().write(buffer, 0, len);
+                    }
+                }
             } else {
-                response.sendRedirect("AreaCheckServlet.java");
+                response.setContentType("text/html;charset=UTF-8");
+                request.getRequestDispatcher("FormPage.jsp").forward(request, response);
             }
-         /*   response.setContentType("text/html");
-            PrintWriter pw=response.getWriter();
-            response.sendRedirect("FormPage.jsp");
-            pw.close();  */
         }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String query = request.getQueryString();
-        if (query == null) {
-            response.sendRedirect("FormPage.jsp");
-        } else {
+      //  String query = request.getQueryString();
+     //   if (query == null) {
+       //     response.sendRedirect("FormPage.jsp");
+       // } else {
             response.sendRedirect("AreaCheckServlet.java");
-        }
+       // }
 
     }
 
