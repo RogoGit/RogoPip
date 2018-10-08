aa30717230e448cc9f8a15767b3581d05bd65497
Nikita Rogalenko <nikitalr@mail.ru>
2018-10-08 19:21:32 +0300

    correct check in AreaCheck Servlet

--- RogoPip2/src/AreaCheckServlet.java
+++ RogoPip2/src/AreaCheckServlet.java
@@ -7,14 +7,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.ParseException;
 
+import static java.lang.Math.pow;
+
 @WebServlet("/checkServ")
 public class AreaCheckServlet extends HttpServlet {
 
 
     public static String CheckArea(double x, double y, double r) {
-        if ((x>=200) && (y>=200) && ((Math.pow((x-200),2) + Math.pow((y-200),2)) <= Math.pow((r*20),2))) {return "Попадает";}
-        if ((x>=200) && (y<=200) && ((200-(200+(y-200)))<=((-2)*(x-200)+r*40))) {return "Попадает";}
-        if ((x<=200) && (y>=200) && (x>=(200-(r*40))) && (y<=(200+(r*20)))) {return "Попадает";}
+        if ((x>=0) && (y>=0) && (y<=((-2)*x+r))) {return "Попадает";}
+        if ((x>=0) && (y<=0) && ((pow(x,2) + pow(y,2))<=pow((r/2),2))) {return "Попадает";}
+        if ((x<=0) && (y<=0) && (x>=(-r)) && (y>=(-r/2))) {return "Попадает";}
         return "Не попадает";
     }
 
