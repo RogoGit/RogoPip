bc376fb7ab1e487d9139b7679fe63ba743fac8d1
Nikita Rogalenko <nikitalr@mail.ru>
2018-11-09 09:15:55 +0300

    gradle fixed - primefaces work

---
+++ Weblab3/web3/src/main/java/org/rogisa/beans/ServerTimeBean.java
@@ -0,0 +1,17 @@
+package org.rogisa.beans;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.Serializable;
+
+public class ServerTimeBean implements Serializable{
+    private String time;
+    
+    public String getTime() {
+        return time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-uuuu hh:mm:ss"));
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ Weblab3/web3/src/main/java/org/rogisa/beans/SpinnerViewBean.java
@@ -0,0 +1,18 @@
+package org.rogisa.beans;
+
+import java.io.Serializable;
+
+public class SpinnerViewBean implements Serializable {
+
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+}
+
