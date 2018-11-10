8d763d9c2cc2394b761098bf3e94c95065e43d28
Nikita Rogalenko <nikitalr@mail.ru>
2018-11-10 13:15:24 +0300

    wip wip

--- Weblab3/web3/src/main/webapp/MainPageScripts.js
+++ Weblab3/web3/src/main/webapp/MainPageScripts.js
@@ -3,18 +3,25 @@ var dotArray = []; /// пусть будет пока
 var inputRad = document.getElementById("fieldsForm:rad");
 var oldRad = 1;
 
+function getMousePos() {
+
+    return {
+        x: (event.clientX - 390) / 180,
+        y: -(event.clientY - 340) / 180
+    };
+
+}
+
 function makeNewDot() {
 
-        if (!(isNaN(inputRad.value) || inputRad.value < 1 || inputRad.value > 4)) {
-            var kx = (event.clientX - 390) / 180;
-            var ky = -(event.clientY - 340) / 180;
-            var radius = inputRad.value;
+    var aX = document.getElementById("AreaForm:AreaX");
+    var aY = document.getElementById("AreaForm:AreaY");
+    var aR = document.getElementById("AreaForm:AreaR");
+    aX.value = getMousePos().x;
+    aY.value = getMousePos().y;
+    aR.value = inputRad.value;
+    document.forms["AreaForm"].submit();
 
-        } else {
-
-           // document.getElementById("errors").classList.remove("hidden");
-            //document.getElementById("errors").innerHTML = "Введите правильный радиус!";
-        }
 }
 
 function resizeArea() {
