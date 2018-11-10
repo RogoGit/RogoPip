7802063645f51ec9d298750bc4dc5ea687340cac
Isaev Alex <imashkaman@gmail.com>
2018-11-10 12:31:03 +0300

    Added maven and restructure project.

---
+++ Weblab3/src/main/java/org/rogisa/beans/DotMaker.java
@@ -0,0 +1,80 @@
+package main.java.org.rogisa.beans;
+
+import java.io.Serializable;
+
+public class DotMaker implements Serializable {
+    private double kx;
+    private double ky;
+    private double rad;
+    private String color;
+    private String res;
+
+    public DotMaker(double kx, double ky, double rad, String color, String res) {
+        this.kx = kx;
+        this.ky = ky;
+        this.rad = rad;
+        this.color = color;
+        this.res = res;
+    }
+
+    public DotMaker() {
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getKx() {
+        return kx;
+    }
+
+    public void setKx(double kx) {
+        this.kx = kx;
+    }
+
+    public double getKy() {
+        return ky;
+    }
+
+    public void setKy(double ky) {
+        this.ky = ky;
+    }
+
+    public double getRad() {
+        return rad;
+    }
+
+    public void setRad(double rad) {
+        this.rad = rad;
+    }
+
+    public String getRes() {
+        return res;
+    }
+
+    public void setRes(String res) {
+        this.res = res;
+    }
+
+    public void areaCheck() {
+
+        boolean ifHit =  (this.kx <= 0 && this.ky <= 0 && -this.kx <= this.rad/2 && -this.ky <= this.rad) ||
+                        (this.kx <= 0 && this.ky >= 0 && (this.kx * this.kx + this.ky * this.ky) <= (this.rad * this.rad)/4) ||
+                        (this.kx >= 0 && this.ky <= 0 && this.ky>=(-this.kx-this.rad)); // FIX IT!!!
+
+        if (ifHit) {
+            this.color = "green";
+            this.res = "Попадает";
+        } else {
+            this.color = "red";
+            this.res = "Не попадает";
+        }
+
+    }
+
+}
